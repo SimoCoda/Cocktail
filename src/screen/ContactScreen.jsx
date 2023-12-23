3f88781e37bfa19174c658914531448660824a3b
Simone <codarinsimone@gmail.com>
2023-12-23 14:00:56 +0100

    Modifica contattami e inizio configurazione homepage

--- src/screen/ContactScreen.jsx
+++ src/screen/ContactScreen.jsx
@@ -1,8 +1,91 @@
-import React from "react";
 import { Hero } from "../components";
 import backImage from "../assets/image/Contact-hero.png";
 const ContactScreen = () => {
-  return <>ContactScreen</>;
+
+  return (
+    <>
+      <Hero img={backImage} disableOverlay>
+        <div className="container-hero container">
+          <div className="contact-hero-text">
+            <div className="contact-hero-title">
+              <h2 className="contact-title">
+                Vorresti Aggiungere un Cocktail?
+              </h2>
+              <h4 className="contact-subtitle">
+                Il nostro team è sempre disponibile per valutare possibili nuove
+                ricette ed ad aggiungerle alla nostra database.
+              </h4>
+            </div>
+          </div>
+          <div className="contact-form-container container">
+            <form
+              className="contact-form container"
+              action="https://formspree.io/f/xpzvakjl"
+              method="POST"
+            >
+              <div className="form-group">
+                <label htmlFor="nome">Nome</label>
+                <input
+                  type="text"
+                  id="nome"
+                  name="name"
+                  className="input"
+                  placeholder="Daniele"
+                />
+                <hr />
+              </div>
+              <div className="form-group">
+                <label htmlFor="cognome">Cognome</label>
+                <input
+                  type="text"
+                  id="Cognome"
+                  name="Cognome"
+                  className="input"
+                  placeholder="Tamarindo"
+                />
+                <hr />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="input"
+                  placeholder="daniele@esempio.com"
+                />
+                <hr />
+              </div>
+              <div className="form-group">
+                <label htmlFor="cellulare">Cellulare</label>
+                <input
+                  type="cel"
+                  id="cellulare"
+                  name="cellulare"
+                  className="input"
+                  placeholder="555 555-5555"
+                />
+                <hr />
+              </div>
+              <div className="form-group">
+                <label htmlFor="ricetta">La tua ricetta</label>
+                <input
+                  type="tel"
+                  id="ricetta"
+                  name="ricetta"
+                  className="input"
+                  placeholder="Descrivi la tua ricetta"
+                />
+                <hr />
+              </div>
+              <br />
+              <button className="btn btn-primary" type="submit">Invia Ricetta</button>
+            </form>
+          </div>
+        </div>
+      </Hero>
+    </>
+  );
 };
 
 export default ContactScreen;
--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -1,11 +1,43 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Hero, Cocktails, Loading, ErrorMessage } from "../components";
 import { FaSearch } from "react-icons/fa";
 import Lottie from "react-lottie";
 import animationData from "../assets/animation/drink-animation.json";
 import { Link } from "react-router-dom";
 const HomeScreen = () => {
-  return <>Home Screen</>;
+  return(
+  <>
+    <Hero>
+      <div className="home-hero">
+        <div className="home-hero-text">
+          <div className="home-hero-title">
+            <h2 className="brand-color">WIKI DRINK</h2>
+            <h4>Tutti i drink a portata di un click</h4>            
+          </div>
+          <p>
+            Wiki Drink è un database internazionale che mette a tua disposizione, in maniera <span className="brand-color">gratuita</span>, le ricette dei più importanti
+            e diffusi cocktail al mondo.
+          </p>
+          <Link to="/about" className="btn btn-primary">Scopri di più</Link>
+        </div>
+        <div className="home-hero-img">
+          <Lottie
+            options={{
+              loop: true,
+              autoplay: true,
+              reverse: true,
+              animationData,
+              renderSettings:{
+                preserveAspectRatio: 'xMidYMid slice',
+              }
+            }}
+            heigth={350}
+          />
+        </div>
+      </div>
+    </Hero>
+  </>
+  )
 };
 
 export default HomeScreen;
